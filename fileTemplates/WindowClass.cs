969214ffb13657b8e3001cc7867e019a2aaa6411
szx <810143012@qq.com>
2021-03-02 20:24:11 +0800

    RD-2019.1.1 <szx@DESKTOP-CVI5GRH Create emmy.xml, WindowClass.cs, ide.general.xml, customization.xml, _@user_Rider Dark.icls, project.default.xml, vsts_settings.xml, laf.xml, jdk.table.xml, Default.xml, codeLens.xml, ReSharper copy.xml, find.xml, ui.lnf.xml, keymap.xml, vcs.xml, editor.codeinsight.xml, colors.scheme.xml, editor.xml, debugger.xml, WindowClass.cs, updates.xml

---
+++ fileTemplates/WindowClass.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ${NAME} : BaseWindow
+{
+    //------------------控件定义Start------------------//
+    //------------------控件定义End--------------------//
+    
+    public override WinLayer layer => WinLayer.Normal;
+
+    public override string resName => throw new Exception("未定义资源名");
+
+    protected override void OnCreate()
+    {
+    }
+    
+    protected override void OnOpen(object param)
+    {
+    }
+    
+    protected override void OnClose(bool cache)
+    {
+    }
+    
+    
+}
